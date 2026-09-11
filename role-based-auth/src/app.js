70634488495ad2cce217b3a26a7bc2e2868fb4a3
smasim825 <smasim825@gmail.com>
2026-09-11 17:22:09 +0600

    feat(role-based-auth): configure express app & MongoDB Atlas connection

---
+++ role-based-auth/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes');
+const musicRoutes = require('./routes/music.routes');
+
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/music', musicRoutes);
+
+module.exports = app;
